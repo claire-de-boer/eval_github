e344bf43f7489e7d37e45ab908dae085bad570f7
Claire De Boer <claire.deboer.chorew@gmail.com>
2026-03-19 15:39:15 +0100

    Add files via upload

---
+++ perceptron.py
@@ -0,0 +1,79 @@
+"""
+PROJET : PERCEPTRON COLLABORATIF
+EQUIPE : 6 DEVELOPPEURS
+"""
+
+# --- PERSONNE 2 : feature activation function ---
+def hs(x):
+    """
+    TODO: Retourner 1 si x > 0, sinon 0.
+    """
+    pass
+
+def relu(x):
+    """
+    TODO: Retourner x si x > 0, sinon 0.
+    """
+    pass
+
+
+# --- PERSONNE 1 : feature input ---
+def get_data(filename):
+    """
+    TODO: Lire le fichier filename.
+    Retourner une liste de points [[x1, x2], ...] et une liste de labels [y, ...].
+    """
+    pass
+
+
+# --- PERSONNE 3 : feature prediction ---
+def predict(point, w1, w2, b):
+    """
+    TODO: Calculer la somme pondérée z = w1*x1 + w2*x2 + b.
+    Passer z dans la fonction hs() de la Personne 2 et retourner le résultat.
+    """
+    pass
+
+
+# --- PERSONNE 5 : feature output ---
+def print_predict_all(points, labels, w1, w2, b):
+    """
+    TODO: Pour chaque point, afficher "Attendu: y | Prédit: y_hat".
+    Afficher les poids finaux w1, w2 et b arrondis à 4 décimales.
+    """
+    pass
+
+
+# --- MAIN ENGINE (PERSONNE 4 : feature train) ---
+if __name__ == "__main__":
+    # Paramètres d'apprentissage
+    lr = 0.1
+    epochs = 20
+    
+    # 1. Chargement des données (Utilisé par P4, codé par P1)
+    # points, labels = get_data("data.txt")
+    
+    # 2. Initialisation des poids (w1, w2, b à 0.0)
+    w1, w2, b = 0.0, 0.0, 0.0
+
+    print("Début de l'entraînement...")
+
+    for iteration in range(10000) :
+        y_hat = predict(points, w1, w2, b)
+        error = target - y_hat
+        
+        grads_w2 = []
+        for j in range(len(points)):
+            w_temp = w2[j] + epochs
+            y_temp = predict(points, w1, w_temp, b)
+            error_temp = y_temp - y_true
+            grad = (error_temp - error) / epochs
+            grads_w2.append(grad)
+
+        for j in range(len(points)):
+            w2[j] -= lr * grads_w2[j]
+    
+    print("Entraînement terminé.")
+
+    # 3. Affichage des résultats (Appel de la fonction de P5)
+    # print_predict_all(points, labels, w1, w2, b)
